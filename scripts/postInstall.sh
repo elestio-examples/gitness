7a8bfd97bd1064e6c96599b171bb9ef511855d06
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-09-26 09:20:48 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -4,3 +4,16 @@ set -o allexport; source .env; set +o allexport;
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
 sleep 30s;
+
+target=$(docker-compose port gitness 3000)
+
+curl http://${target}/api/v1/register?include_cookie=true \
+  -H 'accept: */*' \
+  -H 'accept-language: fr-FR,fr;q=0.9,en-US;q=0.8,en;q=0.7,he;q=0.6' \
+  -H 'authorization;' \
+  -H 'cache-control: no-cache' \
+  -H 'content-type: application/json' \
+  -H 'pragma: no-cache' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36' \
+  --data-raw '{"display_name":"root","email":"'${ADMIN_EMAIL}'","uid":"root","password":"'${ADMIN_PASSWORD}'"}' \
+  --compressed
